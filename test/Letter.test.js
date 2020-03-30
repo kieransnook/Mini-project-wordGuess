cfa4c401ad88e39afd4c9bb55ee0a5b4a4162d49
SamSherrill <38333695+SamSherrill@users.noreply.github.com>
2020-03-30 14:25:21 -0400

    additional changes

--- test/Letter.test.js
+++ test/Letter.test.js
@@ -3,6 +3,8 @@ const Letter = require("../lib/Letter");
 describe("Letter class", () => {
   it("Characters that aren't digits or letters are instantly visible", () => {
     expect(new Letter("?").visible).toBe(true);
+    
+
   });
 
   it("toString returns _ for letters", () => {

f58a01fe9182394b04482d6a4769e4954d90e711
SamSherrill <38333695+SamSherrill@users.noreply.github.com>
2020-03-30 13:19:41 -0400

    initial files & pseudo code

---
+++ index.js
@@ -0,0 +1,26 @@
+// * **index.js**: The file containing the logic for the course of the game, which depends on `Word.js` and:
+//   * Randomly selects a word and uses the `Word` class to store it
+//   * Prompts the user for each guess and keeps track of the user’s remaining guesses
+// 3. `Letter.js` *should not* `require` any other files.
+// 4. `Word.js` *should only* require `Letter.js`
+// 5. **HINT:** Write `Letter.js` first and test it on its own before moving on, then do the same thing with `Word.js`
+//create Letter class
+// turn phrase into an array []
+
+// **** FROM README ****
+
+// * **index.js**: The file containing the logic for the course of the game, which depends on `Word.js` and:
+//   * Randomly selects a word and uses the `Word` class to store it
+//   * Prompts the user for each guess and keeps track of the user's remaining guesses
+
+// 3. `Letter.js` *should not* `require` any other files.
+
+// 4. `Word.js` *should only* require `Letter.js`
+
+// 5. **HINT:** Write `Letter.js` first and test it on its own
+//      before moving on, then do the same thing with `Word.js`
+
+// ### Hints
+// * If you name your letter's display function `toString`, JavaScript
+//  will call that function automatically whenever casting that object
+//  to a string (check out this example: <https://jsbin.com/facawetume/edit?js,console>)
